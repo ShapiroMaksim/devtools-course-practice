78dcf935309aef8b87ad9f909ba9a7cca2dbc77f
ShapiroMaksim <Shapirom1999@gmail.com>
2020-10-25 14:06:36 +0300

    another style fix

--- modules/figure-capacity/test/test_figure_capacity.cpp
+++ modules/figure-capacity/test/test_figure_capacity.cpp
@@ -1,8 +1,8 @@
 // Copyright 2020 Shapiro Maksim
-#include <gtest/gtest.h>
 #include "include/figure_capacity.h"
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <gtest/gtest.h>
 
 TEST(Sphere, Can_create_sphere) {
     // Arrange & Act & Assert
@@ -20,16 +20,16 @@ TEST(Sphere, Can_create_sphere_with_rational_parameter) {
 }
 
 TEST(Sphere, Can_get_radius_of_sphere) {
-    // Arrange & Act 
+    // Arrange & Act
     Sphere s(1);
-    //Assert
+    // Assert
     EXPECT_EQ(1, s.getRadius());
 }
 
 TEST(Sphere, Can_get_radius_of_sphere_with_another_parameter) {
-    // Arrange & Act 
+    // Arrange & Act
     Sphere s(1.2);
-    //Assert
+    // Assert
     EXPECT_EQ(1.2, s.getRadius());
 }
 
@@ -47,8 +47,8 @@ TEST(Sphere, Cant_calculate_capacity_of_sphere) {
     // Arrange
     Sphere s(5);
     double res;
-    //Act
+    // Act
     res = s.capacity();
-    //Assert
+    // Assert
     EXPECT_EQ(5 * 5 * 5 * M_PI * 4 / 3, res);
 }
